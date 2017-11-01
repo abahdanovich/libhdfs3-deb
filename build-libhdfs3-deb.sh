fdce09515e951245cd712eb7b71d3039762fffea
Noah Levitt <nlevitt@archive.org>
2017-11-01 22:04:45 +0000

    add support for trusty

--- build-libhdfs3-deb.sh
+++ build-libhdfs3-deb.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
+
+os_release=$(lsb_release -sc)
+if [ "$os_release" != trusty ] && [ "$os_release" != xenial ] ; then
+    echo "only ubuntu trusty (14.04) or xenial (16.04) are supported (you are on $os_release)"
+    exit 1
+fi
+
+platform=$(uname -i)
+if [ "$platform" != x86_64 ] ; then
+    echo "only x86_64 is supported (you are on $platform)"
+    exit 1
+fi
+
 workdir=$(mktemp -d)
 cd $workdir
 wget -q https://repo.continuum.io/miniconda/Miniconda3-4.3.27.1-Linux-x86_64.sh
@@ -8,10 +21,15 @@ echo '640f505f06f87d75bebc629e4a677ebb185ea9a34eb6d7c199db0753ffc42f62  Minicond
 bash Miniconda3-4.3.27.1-Linux-x86_64.sh -b -p ./miniconda -u
 ./miniconda/bin/conda install -q -y -c conda-forge libhdfs3
 mkdir -p libhdfs3_1-1/usr/lib/x86_64-linux-gnu/
-rsync -l ./miniconda/lib/libprotobuf.* ./libhdfs3_1-1/usr/lib/x86_64-linux-gnu/
-rsync -l ./miniconda/lib/libhdfs* ./libhdfs3_1-1/usr/lib/x86_64-linux-gnu/
+rsync -l ./miniconda/lib/libprotobuf.so.* ./libhdfs3_1-1/usr/lib/x86_64-linux-gnu/
+rsync -l ./miniconda/lib/libhdfs3.so* ./libhdfs3_1-1/usr/lib/x86_64-linux-gnu/
 
 # create metadata items for .deb package
+if [ "$os_release" = "xenial" ] ; then
+    depends="libasn1-8-heimdal, libc6, libcomerr2, libcurl3, libffi6, libgcc1, libgmp10, libgnutls30, libgsasl7, libgssapi-krb5-2, libgssapi3-heimdal, libhcrypto4-heimdal, libheimbase1-heimdal, libheimntlm0-heimdal, libhogweed4, libhx509-5-heimdal, libicu55, libidn11, libk5crypto3, libkeyutils1, libkrb5-26-heimdal, libkrb5-3, libkrb5support0, libldap-2.4-2, liblzma5, libnettle6, libntlm0, libp11-kit0, libroken18-heimdal, librtmp1, libsasl2-2, libsqlite3-0, libssl1.0.0, libstdc++6, libtasn1-6, libuuid1, libwind0-heimdal, libxml2, zlib1g"
+else # trusty
+    depends="libasn1-8-heimdal, libc6, libcomerr2, libcurl3, libffi6, libgcc1, libgcrypt11, libgnutls26, libgpg-error0, libgsasl7, libgssapi3-heimdal, libgssapi-krb5-2, libhcrypto4-heimdal, libheimbase1-heimdal, libheimntlm0-heimdal, libhx509-5-heimdal, libidn11, libk5crypto3, libkeyutils1, libkrb5-26-heimdal, libkrb5-3, libkrb5support0, libldap-2.4-2, liblzma5, libntlm0, libp11-kit0, libroken18-heimdal, librtmp0, libsasl2-2, libsqlite3-0, libssl1.0.0, libstdc++6, libtasn1-6, libuuid1, libwind0-heimdal, libxml2, zlib1g"
+fi
 mkdir libhdfs3_1-1/DEBIAN
 cat >libhdfs3_1-1/DEBIAN/control <<EOF
 Package: libhdfs3
@@ -19,14 +37,15 @@ Version: 1-1
 Section: utils
 Priority: optional
 Architecture: amd64
-Depends: libasn1-8-heimdal, libc6, libcomerr2, libcurl3, libffi6, libgcc1, libgmp10, libgnutls30, libgsasl7, libgssapi-krb5-2, libgssapi3-heimdal, libhcrypto4-heimdal, libheimbase1-heimdal, libheimntlm0-heimdal, libhogweed4, libhx509-5-heimdal, libicu55, libidn11, libk5crypto3, libkeyutils1, libkrb5-26-heimdal, libkrb5-3, libkrb5support0, libldap-2.4-2, liblzma5, libnettle6, libntlm0, libp11-kit0, libroken18-heimdal, librtmp1, libsasl2-2, libsqlite3-0, libssl1.0.0, libstdc++6, libtasn1-6, libuuid1, libwind0-heimdal, libxml2, zlib1g
+Depends: $depends
 Maintainer: James Kafader <jkafader@archive.org>
 Description: Binary package for libhdfs3
  Package to avoid having to install anaconda/miniconda and competing python installations.
 EOF
 
 dpkg-deb --build libhdfs3_1-1
-mv libhdfs3_1-1.deb /tmp
+dest="/tmp/libhdfs3_1-1-$os_release-$platform.deb"
+mv libhdfs3_1-1.deb $dest
 cd /tmp
 rm -R $workdir
-echo "built deb file: /tmp/libhdfs3_1-1.deb"
+echo "built deb file: $dest"
